90a94f01382ac2b2212fb47a930d05d591ced7d3
Ben2023-max <benardrangala@gmail.com>
2026-05-23 12:09:06 +0300

    Add manifest.js for Gem Self Help Group

---
+++ manifest.js
@@ -0,0 +1,16 @@
+{
+  "name": "Gem Self Help Group",
+  "short_name": "GEM SHG",
+  "start_url": "index.html",
+  "display": "standalone",
+  "background_color": "#f1f3f4",
+  "theme_color": "#0F9D58",
+  "icons": [
+    {
+      "src": "https://i.ibb.co/0VpYm8K/gem-logo.png",
+      "sizes": "512x512",
+      "type": "image/png",
+      "purpose": "any maskable"
+    }
+  ]
+}
